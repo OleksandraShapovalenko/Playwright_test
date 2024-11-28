b50e161302777484da99f9b529e5a0aaf0e34a9b
Oleksandra Shapovalenko <shapovalenko.aleksandra@gmail.com>
2024-11-28 10:37:41 +0200

    Playwright_API

---
+++ playwright/fixtures/userPage.ts
@@ -0,0 +1,30 @@
+import { test as base, expect } from "@playwright/test";
+import dotenv from "dotenv";
+import path from 'path';
+
+const authFile = path.join(__dirname, '../.auth/user.json');
+
+export const test = base.extend<{
+    userGarage: any; 
+    loggedInPage: any;
+  }>({
+    userGarage: async ({ page }, use) => {
+
+        await page.goto('https://qauto.forstudy.space/panel/garage');
+
+        await use(page);
+    
+        await page.close();
+      },
+    loggedInPage: async ({ page }, use) => {
+
+      await page.goto('/');
+      await use(page);
+  
+      await page.close();
+    },
+    });
+    
+  test.use({ storageState: authFile })
+
+  export { expect } from '@playwright/test';
